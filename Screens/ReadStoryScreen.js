4ba5b9e215954e82d75fcca23fca2aca8811c17a
pavankarthik1023 <64345984+pavankarthik1023@users.noreply.github.com>
2021-02-05 15:00:52 +0530

    Add files via upload

---
+++ Screens/ReadStoryScreen.js
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.allText}>
+        <TouchableOpacity style={styles.header}>
+          <Text style={styles.headerText}>Story Hub App</Text>
+        </TouchableOpacity>
+
+        <Text style={styles.displayText}>
+          😞 Oops, it seems that this screen is not ready to show you yet. 😞
+          You can read stories in this screen in the upcoming versions of this
+          app.
+        </Text>
+
+        <Text style={styles.displayText}>
+          But for now, you can read some moral stories by just pasting this link
+          in a new tab - " https://wealthygorilla.com/best-short-moral-stories/
+          " Hope, you will love !
+        </Text>
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  allText: {
+    backgroundColor: '#ef7b64',
+    flex: 1,
+    fontFamily: 'comic sans ms',
+  },
+  header: {
+    backgroundColor: 'pink',
+    border: 'dashed',
+  },
+  headerText: {
+    fontFamily: 'britannic',
+    fontSize: 35,
+    textAlign: 'center',
+    padding: 5,
+  },
+  displayText: {
+    fontFamily: 'britannic',
+    fontSize: 20,
+    padding: 15,
+  },
+});
